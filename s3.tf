3f2b7e35836fc0e3a518554375993644c2f2e0af
magako <qutianh@gmail.com>
2023-01-29 23:53:38 +0900

    :sparkles: feat: add s3 public bucket

--- s3.tf
+++ s3.tf
@@ -36,3 +36,26 @@ resource "aws_s3_bucket_public_access_block" "private" {
   ignore_public_acls      = true
   restrict_public_buckets = true
 }
+
+# ------------------------------
+# S3 public bucket
+# ------------------------------
+resource "aws_s3_bucket" "public" {
+  bucket = "${var.project}-${var.environment}-public-bucket-${random_string.s3_unique_key.result}"
+}
+
+resource "aws_s3_bucket_acl" "public-s3-bucket" {
+  bucket = aws_s3_bucket.public.id
+  acl    = "public-read"
+}
+
+resource "aws_s3_bucket_cors_configuration" "public-s3-bucket" {
+  bucket = aws_s3_bucket.public.id
+
+  cors_rule {
+    allowed_origins = ["https://example.com"]
+    allowed_methods = ["GET"]
+    allowed_headers = ["*"]
+    max_age_seconds = 3000
+  }
+}
