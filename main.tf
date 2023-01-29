8d7bb982de46f4077a159d4cd947a68cda7966de
magako <qutianh@gmail.com>
2023-01-29 18:38:04 +0900

    feat: terraform configure

---
+++ main.tf
@@ -0,0 +1,26 @@
+# ------------------------------
+# Terraform configuration
+# ------------------------------
+terraform {
+  required_version = ">=0.13"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~>3.0"
+    }
+  }
+  backend "s3" {
+    bucket  = "tastylog-tfstate-bucket-magata"
+    key     = "tastylog-dev.tfstate"
+    region  = "ap-northeast-1"
+    profile = "terraform"
+  }
+}
+
+# ------------------------------
+# Provider
+# ------------------------------
+provider "aws" {
+  profile = "terraform"
+  region  = "ap-northeast-1"
+}
