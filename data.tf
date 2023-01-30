139021cc7f6d2c53068cd8feafe8ed77151cff81
magako <qutianh@gmail.com>
2023-01-30 22:05:53 +0900

    :sparkles: feat add s3 log bucket

---
+++ data.tf
@@ -0,0 +1,2 @@
+# AWS ELB のサービス AWS アカウント（リージョン共通）
+data "aws_elb_service_account" "service_account" {}
--- s3.tf
+++ s3.tf
@@ -59,3 +59,42 @@ resource "aws_s3_bucket_cors_configuration" "public-s3-bucket" {
     max_age_seconds = 3000
   }
 }
+
+# ------------------------------
+# S3 log bucket
+# ------------------------------
+resource "aws_s3_bucket" "alb_log" {
+  bucket = "${var.project}-${var.environment}-alb-log-bucket-${random_string.s3_unique_key.result}"
+}
+
+resource "aws_s3_bucket_lifecycle_configuration" "alb_log_bucket" {
+  bucket = aws_s3_bucket.alb_log.id
+
+  rule {
+    id = "expiration-rule"
+
+    status = "Enabled"
+
+    expiration {
+      days = "180"
+    }
+  }
+}
+
+resource "aws_s3_bucket_policy" "alb_log" {
+  bucket = aws_s3_bucket.alb_log.id
+  policy = data.aws_iam_policy_document.alb_log.json
+}
+
+data "aws_iam_policy_document" "alb_log" {
+  statement {
+    effect    = "Allow"
+    actions   = ["s3:PutObject"]
+    resources = ["arn:aws:s3:::${aws_s3_bucket.alb_log.id}/*"]
+
+    principals {
+      type        = "AWS"
+      identifiers = [data.aws_elb_service_account.service_account.arn]
+    }
+  }
+}
