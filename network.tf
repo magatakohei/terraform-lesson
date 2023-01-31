c8b85b3ea6b08f89463282fe289c61f12010272c
magako <qutianh@gmail.com>
2023-01-31 23:04:28 +0900

    :sparkles: feat: Multi-AZ for private subnets

--- network.tf
+++ network.tf
@@ -91,6 +91,18 @@ resource "aws_subnet" "private_1a" {
   }
 }
 
+resource "aws_subnet" "private_1c" {
+  vpc_id                  = aws_vpc.example.id
+  cidr_block              = "10.0.66.0/24"
+  availability_zone       = "ap-northeast-1c"
+  map_public_ip_on_launch = false
+  tags = {
+    Name    = "${var.project}-${var.environment}-private-subnet-1c"
+    Project = var.project
+    Env     = var.environment
+  }
+}
+
 resource "aws_eip" "nat_gateway_1a" {
   vpc        = true
   depends_on = [aws_internet_gateway.example]
@@ -102,6 +114,22 @@ resource "aws_nat_gateway" "nat_gateway_1a" {
   depends_on    = [aws_internet_gateway.example]
   tags = {
     Name    = "${var.project}-${var.environment}-nat-1a"
+    Project = var.project
+    Env     = var.environment
+  }
+}
+
+resource "aws_eip" "nat_gateway_1c" {
+  vpc        = true
+  depends_on = [aws_internet_gateway.example]
+}
+
+resource "aws_nat_gateway" "nat_gateway_1c" {
+  allocation_id = aws_eip.nat_gateway_1c.id
+  subnet_id     = aws_subnet.public_1c.id
+  depends_on    = [aws_internet_gateway.example]
+  tags = {
+    Name    = "${var.project}-${var.environment}-nat-1c"
     Project = var.project
     Env     = var.environment
   }
@@ -122,7 +150,27 @@ resource "aws_route" "private_1a" {
   destination_cidr_block = "0.0.0.0/0"
 }
 
+resource "aws_route_table" "private_1c" {
+  vpc_id = aws_vpc.example.id
+  tags = {
+    Name    = "${var.project}-${var.environment}-private-1c-route-table"
+    Project = var.project
+    Env     = var.environment
+  }
+}
+
+resource "aws_route" "private_1c" {
+  route_table_id         = aws_route_table.private_1c.id
+  nat_gateway_id         = aws_nat_gateway.nat_gateway_1c.id
+  destination_cidr_block = "0.0.0.0/0"
+}
+
 resource "aws_route_table_association" "private_1a" {
   subnet_id      = aws_subnet.private_1a.id
   route_table_id = aws_route_table.private_1a.id
+}
+
+resource "aws_route_table_association" "private_1c" {
+  subnet_id      = aws_subnet.private_1c.id
+  route_table_id = aws_route_table.private_1c.id
 }
