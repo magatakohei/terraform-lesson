d643628525e5772f1cb2483165babb52b07228a0
magako <qutianh@gmail.com>
2023-02-04 01:00:42 +0900

    ✨ feat: alb target group

--- elb.tf
+++ elb.tf
@@ -93,6 +93,49 @@ resource "aws_alb_listener" "redirect_http_to_https" {
   }
 }
 
+resource "aws_lb_listener_rule" "https_to_target_group" {
+  listener_arn = aws_alb_listener.https.arn
+  priority     = 100
+
+  action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.ecs_fargate.arn
+  }
+
+  condition {
+    path_pattern {
+      values = ["/*"]
+    }
+  }
+}
+
+# ------------------------------
+# target group
+# ------------------------------
+resource "aws_lb_target_group" "ecs_fargate" {
+  name                 = "${var.environment}-ecs-fargate"
+  target_type          = "ip"
+  vpc_id               = aws_vpc.example.id
+  port                 = 80
+  protocol             = "HTTP"
+  deregistration_delay = 300
+
+  health_check {
+    path                = "/"
+    healthy_threshold   = 5
+    unhealthy_threshold = 2
+    timeout             = 5
+    interval            = 30
+    matcher             = 200
+    port                = "traffic-port"
+    protocol            = "HTTP"
+  }
+
+  depends_on = [
+    aws_lb.example
+  ]
+}
+
 output "alb_dns_name" {
   value = aws_lb.example.dns_name
 }
