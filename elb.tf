ff3d11fbf289789ac17080c59529eee0310b7f41
magako <qutianh@gmail.com>
2023-02-04 00:33:49 +0900

    ✨ feat: add https listener

--- elb.tf
+++ elb.tf
@@ -59,6 +59,40 @@ resource "aws_alb_listener" "http" {
   }
 }
 
+resource "aws_alb_listener" "https" {
+  load_balancer_arn = aws_lb.example.arn
+  port              = "443"
+  protocol          = "HTTPS"
+  certificate_arn   = aws_acm_certificate.tokyo_cert.arn
+  ssl_policy        = "ELBSecurityPolicy-2016-08"
+
+  default_action {
+    type = "fixed-response"
+
+    fixed_response {
+      content_type = "text/plain"
+      message_body = "これは「HTTPS」です"
+      status_code  = 200
+    }
+  }
+}
+
+resource "aws_alb_listener" "redirect_http_to_https" {
+  load_balancer_arn = aws_lb.example.arn
+  port              = "8080"
+  protocol          = "HTTP"
+
+  default_action {
+    type = "redirect"
+
+    redirect {
+      port        = "443"
+      protocol    = "HTTPS"
+      status_code = "HTTP_301"
+    }
+  }
+}
+
 output "alb_dns_name" {
   value = aws_lb.example.dns_name
 }
