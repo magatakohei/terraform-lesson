b98eeeda89e4e14b645a4531a0d71185d402eb51
magako <qutianh@gmail.com>
2023-02-14 08:33:23 +0900

    🐛 fix:specify iam policy json

--- codebuild.tf
+++ codebuild.tf
@@ -32,7 +32,7 @@ module "codebuild_role" {
   source     = "./iam_role"
   name       = "codebuild"
   identifier = "codebuild.amazonaws.com"
-  policy     = data.aws_iam_policy_document.codebuild
+  policy     = data.aws_iam_policy_document.codebuild.json
 }
 
 # ------------------------------
