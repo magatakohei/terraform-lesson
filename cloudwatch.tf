ef353d24326cb18151546a646abcb79f15d16d5a
magako <qutianh@gmail.com>
2023-02-14 23:38:36 +0900

    ✨ fix: cloudwatch log group

--- cloudwatch.tf
+++ cloudwatch.tf
@@ -2,7 +2,7 @@
 # CloudWatch
 # ------------------------------
 resource "aws_cloudwatch_log_group" "for_ecs" {
-  name              = "/dev/nginx/web"
+  name              = "/dev/app/web"
   retention_in_days = 180
 }
 
