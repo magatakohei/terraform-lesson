3331d18192d2f160bb68199dd80b9103c2dcb81f
magako <qutianh@gmail.com>
2023-02-18 22:50:49 +0900

    ✨ feat: add session manager

--- ec2.tf
+++ ec2.tf
@@ -56,3 +56,24 @@ resource "aws_instance" "example_for_operation" {
 output "operation_instance_id" {
   value = aws_instance.example_for_operation.id
 }
+
+# ------------------------------
+# ssm
+# ------------------------------
+resource "aws_ssm_document" "session_manager_run_shell" {
+  name            = "SSM-SessionManagerRunShell"
+  document_type   = "Session"
+  document_format = "JSON"
+
+  content = <<EOF
+  {
+    "schemaVersion": "1.0",
+    "description": "Document to hold regional settings for Session Manager",
+    "sessionType": "Standard_Stream",
+    "inputs": {
+      "s3BucketName": "${aws_s3_bucket.operation.id}",
+      "cloudWatchLogGroupName": "${aws_cloudwatch_log_group.operation.name}"
+    }
+  }
+  EOF
+}
