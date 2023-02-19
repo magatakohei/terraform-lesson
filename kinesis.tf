c6d5289f704e6ee6890c6822b9c540fa496f9499
magako <qutianh@gmail.com>
2023-02-19 16:50:54 +0900

    ✨ feat: fix kinesis data compression

--- kinesis.tf
+++ kinesis.tf
@@ -33,11 +33,12 @@ module "kinesis_data_firehose_role" {
 # ------------------------------
 resource "aws_kinesis_firehose_delivery_stream" "example" {
   name        = "example"
-  destination = "s3"
+  destination = "extended_s3"
 
-  s3_configuration {
-    role_arn   = module.kinesis_data_firehose_role.iam_role_arn
-    bucket_arn = aws_s3_bucket.cloudwatch_logs.arn
-    prefix     = "ecs-scheduled-tasks/example/"
+  extended_s3_configuration {
+    role_arn           = module.kinesis_data_firehose_role.iam_role_arn
+    bucket_arn         = aws_s3_bucket.cloudwatch_logs.arn
+    prefix             = "ecs-scheduled-tasks/example/"
+    compression_format = "GZIP"
   }
 }
